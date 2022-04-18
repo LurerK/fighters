42e66fd480f17bbe893a5cda283f3ee2efae260e
LurerK <103966032+LurerK@users.noreply.github.com>
2022-04-18 17:34:36 -0400

    Update client.py

--- client.py
+++ client.py
@@ -5,15 +5,16 @@ userInput=input("Type 1 to Upload a file or type 2 to enter fighters manually:")
 if (userInput==1):
     fileName=input("what file: ")
     print("Username is: " + username)
-    fileText = open('name.txt', ' r')
+    fileText = open(fileName, ' r')
     fileRead = fileText.readlines()
     for i in fileRead:
         print("{}".format(fileRead.strip())
         
-    fileText.close
+"""    fileText.close
 elif(userInput==2):
     size=input("How many fighters")
     for i in np.array[size][3]:
         charName=input("")
         char=
 else:
+"""
